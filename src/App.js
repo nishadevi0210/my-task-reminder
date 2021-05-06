4381fa919d3dfe79cd0ede803683cd9ce7e88af6
Nisha Devi <nishadevi0210@gmail.com>
2021-05-06 18:58:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  let name = "NISHA"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{backgroundColor: "red"}}>hello {name}</h1>
+      
     </div>
   );
 }
